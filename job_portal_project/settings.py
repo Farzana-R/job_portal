99f5dff89a36b8a6e7d56b96266d08de03a7b03e
Farzana-R <farzana.r@techversantinfo.com>
2022-11-02 18:34:28 +0530

    connected postgresql

--- job_portal_project/settings.py
+++ job_portal_project/settings.py
@@ -10,7 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import environ
+
 from pathlib import Path
+
+
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,8 +82,12 @@ WSGI_APPLICATION = 'job_portal_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'job_portal_database',
+        'USER': 'job_portal',
+        'PASSWORD': 'portal',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
