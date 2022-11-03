4ae45cb39c7a9b0f67dab2a1aeb8841b637571bd
Farzana-R <farzana.r@techversantinfo.com>
2022-11-03 12:17:27 +0530

    modified requirements

--- job_portal_project/settings.py
+++ job_portal_project/settings.py
@@ -10,15 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-import environ
+
 import os
 
 from pathlib import Path
 
 
-# Initialise environment variables
-env = environ.Env()
-environ.Env.read_env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
