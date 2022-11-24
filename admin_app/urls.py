3fe3dfcdddb00731b0d11b1e47f6b20b267246bc
vishnu-k-s-techversant <vishnu.ks@techversantinfo.com>
2022-11-24 14:58:34 +0530

    urls file changed

--- admin_app/urls.py
+++ admin_app/urls.py
@@ -2,14 +2,15 @@ from django.urls import path
 from . import views 
 
 
-
 urlpatterns = [   
-    path('', views.Home.as_view(),name='home'),
-    path('about/', views.About.as_view(),name='about'),
-    path('contact/', views.Contact.as_view(),name='contact'),
-    path('user_register/', views.UserRegister.as_view(), name='user_register'),
-    path('company_register/', views.CompanyRegister.as_view(), name='company_register'),
-    path('loginview/', views.loginview, name='loginview'),
-    path('logout',views.logout,name='logout'),
+    path('', views.Home.as_view(), name='home'),
+    path('about/', views.About.as_view(), name='about'),
+    path('contact/', views.Contact.as_view(), name='contact'),
+    path('user-register/', views.UserRegister.as_view(),\
+         name='user_register'),
+    path('company-register/', views.CompanyRegister.as_view(),\
+         name='company_register'),
+    path('login/', views.loginview, name='login'),
+    path('logout/', views.logout, name='logout'),
     # path('about/', views.about,name='about'),
 ]
