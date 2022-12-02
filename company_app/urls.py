9d6ce56efdc485cea56661fe1a8b21a3b620d46d
Farzana-R <farzana.r@techversantinfo.com>
2022-12-02 11:32:16 +0530

    company profile

--- company_app/urls.py
+++ company_app/urls.py
@@ -3,9 +3,11 @@ from .import views
 
 app_name = 'company_app'
 urlpatterns = [
-path('company-register/', views.CompanyRegister.as_view(),\
-        name='company_register'),
-path('login/', views.Login.as_view(), name='login'),
+    path('company_register/', views.CompanyRegister.as_view(), name='company_register'),
+    path('login/', views.Login.as_view(), name='login'),
+    path('company_home', views.Home.as_view(), name='company_home'),
+    path('company-profile/', views.CompanyProfile.as_view(), name='company_profile'),
+    path('update-company-profile/', views.UpdateCompanyProfile.as_view(), name='update_company_profile'), 
 ]
 
 
--- company_app/views.py
+++ company_app/views.py
@@ -1,13 +1,45 @@
-from django.contrib import messages, auth
 from django.conf import settings
-from django.shortcuts import render, redirect
+from django.contrib import messages, auth
 from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
 from django.core.mail import send_mail
+from django.shortcuts import render, redirect
+from django.utils.decorators import method_decorator
 from django.views.generic import View
 
 from . import forms
+from company_app.models import Company
 
-# Create your views here.
+
+class Home(View):
+    def get(self, request):
+        # jobs = Job.objects.all()[:3]
+        return render(request, 'company_app/company_home.html')
+
+
+# view company profile
+@method_decorator(login_required, name='get')
+class CompanyProfile(View):
+    def get(self, request):
+        company_details = Company.objects.get(user=request.user)
+        return render(request, 'company_app/dashboard.html', {'company_details': company_details})
+
+
+# edit company profile
+@method_decorator(login_required, name='get')
+class UpdateCompanyProfile(View):
+    def get(self, request):
+        form = forms.CompanyUpdateProfileForm()
+        return render(request, 'company_app/edit_profile.html', {'form': form})
+
+    def post(self, request):
+        profile = Company.objects.filter(user=request.user).first()
+        form = forms.CompanyUpdateProfileForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            form.save()
+            return redirect('company_app:company_profile')
+        else:
+            return render(request, 'company_app/edit_profile.html', {'form': form})
 
 
 class CompanyRegister(View):
@@ -21,7 +53,7 @@ class CompanyRegister(View):
         company_form = forms.CompanyRegisterForm(request.POST) 
         if company_form.is_valid():
             company_form.save()
-            return redirect('loginview')
+            return redirect('company_app:login')
         context = {'form': company_form}
         return render(request, 'company_app/register.html', context)
 
@@ -42,5 +74,5 @@ class Login(View):
             )
             if user is not None:
                 login(request, user)
-                return redirect('home')
+                return redirect('company_app:company_home')
         return render(request, 'company_app/login.html', context)
